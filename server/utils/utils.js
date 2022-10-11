0e5b1736cfd259c8f205a3fb7c2680bfaadfe55f
yass <jooyassini@gmail.com>
2022-10-11 16:31:55 +0300

    Revert "Mon deuxième commit MIGRATE and comment"
    This reverts commit 39043666d0b89b0ab5e48901a3cc5dae788d43b0.

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -1,5 +1,4 @@
-// Les migrations des tables et de ses données
-export const MIGRATE = true;
+export const MIGRATE = false;
 export const getDateTime = (name = "") => {
   const date = new Date();
   name += `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}_${date.getHours()}.${date.getMinutes()}.${date.getSeconds()}_${date.getMilliseconds()}`;
