39043666d0b89b0ab5e48901a3cc5dae788d43b0
yass <jooyassini@gmail.com>
2022-10-11 11:03:08 +0300

    Mon deuxième commit MIGRATE and comment

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -1,4 +1,5 @@
-export const MIGRATE = false;
+// Les migrations des tables et de ses données
+export const MIGRATE = true;
 export const getDateTime = (name = "") => {
   const date = new Date();
   name += `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}_${date.getHours()}.${date.getMinutes()}.${date.getSeconds()}_${date.getMilliseconds()}`;
