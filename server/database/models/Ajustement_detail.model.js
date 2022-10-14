f837261fc0714be76ed31bf090fb15aa94e00469
yass <jooyassini@gmail.com>
2022-10-14 17:12:09 +0300

    Installation mariadb

--- server/database/models/Ajustement_detail.model.js
+++ server/database/models/Ajustement_detail.model.js
@@ -4,8 +4,8 @@ import db from "../../config/Database.js";
 const Ajustement_detail = db.define(
   "ajustement_detail",
   {
-    qantite_nouveau: { type: DataTypes.DOUBLE(10), allowNull: false },
-    qantite_ancien: { type: DataTypes.DOUBLE(10), allowNull: false },
+    qantite_nouveau: { type: DataTypes.DOUBLE, allowNull: false },
+    qantite_ancien: { type: DataTypes.DOUBLE, allowNull: false },
     status: { type: DataTypes.BOOLEAN, defaultValue: "1" },
   },
   { paranoid: true, freezeTableName: true }
